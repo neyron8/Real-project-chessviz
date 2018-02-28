3ac8d6031a8b8beef031217b6b5d258ffa63a98a
Дураков Николай Александрович <iv721s05@mwp06.csc.local>
2018-02-28 08:45:15 +0700

    Updated Project

--- Chess.c
+++ Chess.c
@@ -1,23 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
+	int i, j;
+	char chessboard[8][8] = {
+		"LHSQKSHL",
+		"PPPPPPPP",
+		"        ",
+		"        ",
+		"        ",
+		"        ",
+		"pppppppp",
+		"lhskqshl" };
+	for (i = 0; i < 8; i++) {
 
-int i, j;
-char chessboard[8][8] = {
-"LHSQKSHL",
-"PPPPPPPP",
-"        ",
-"        ",
-"        ",
-"        ",
-"pppppppp",
-"lhskqshl" };
-for(i=0; i<8; i++) {
-for(j=0; j<8; j++) {
-printf("%c", chessboard[i][j]);
-}
-printf("\n");
-}
-printf("\n");
-return 0;
+		for (j = 0; j < 8; j++) {
+			printf("%c", chessboard[i][j]);
+		}
+		printf("\n");
+	}
+
+	printf("\n");
+	return 0;
 }
