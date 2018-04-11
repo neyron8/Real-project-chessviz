065eba08511df45092feb361daa779125023cc59
neyron8 <32508305+neyron8@users.noreply.github.com>
2018-04-11 02:33:39 +0000

    Update board.c

--- board.c
+++ board.c
@@ -1,13 +1,22 @@
-#include <stdio.h>
+#include <stdio.h>  // CHANGE .H and other
 #include "board.h"
 
-int what_is_piece (char *h, Board board)
+int what_is_piece (char *h, Board *board)
 {
+	int flag = 0;
 	for ( char ltr = 'a'; ltr != 'i'; ltr++) {
 		for (int i = 0; i < 8; i++) {
 			if(h[3] == ltr) {
-				if((h[4] - 48)) {
-					board -> cell[h[3] - 97 ]
-			}	
+				if((h[4] - 48) == i) {
+					if(board -> cell[(h[3] - 97)][i].type == Pawn) {
+						printf("\nPAWN DETECTED");
+						flag = 1;
+						return 1;
+					}
+				}	
+		    }
 		}
+	}
+	if (flag == 0)
+		return 0;
 }
