0a545e1f0d5a5160c65389d5e20c0dcd32221d20
Дураков Николай Александрович <iv721s05@mwp11.csc.local>
2018-04-18 09:14:54 +0700

    move realised

--- board.c
+++ board.c
@@ -3,8 +3,9 @@
 #include <ctype.h>
 #include <stdio.h>
 
-int move (char *string, Board *board) {   // Условия измени
+int move (char *string, Board *board) {
 	int bl_ch1, wh_ch1, bl_dig1, wh_dig1, bl_ch2, wh_ch2, bl_dig2, wh_dig2;
+
 	wh_ch1 = (string[3] - 97);
 	wh_ch2 = (string[9] - 97);
 	bl_ch1 = (string[6] - 97);
@@ -25,11 +26,8 @@ int move (char *string, Board *board) {   // Условия измени
 			board -> cell [wh_dig2][wh_ch2].fill = 1;
 			board -> cell [wh_dig2][wh_ch2].color = White;
 		}
-	}
-			
-	if (board -> cell [wh_dig1][wh_ch1].type == Pawn 
-	&& board -> cell [bl_dig1][bl_ch1].type == Pawn){
-		if ( wh_ch2 - wh_ch1 == 0 && (wh_dig1 - wh_dig2 <= 2) ){
+		if ( bl_ch2 - bl_ch1 == 0 && (bl_dig1 - bl_dig2 <= 2) ){
+
 			board -> cell [bl_dig1][bl_ch1].fill = 0;
 
 			board -> cell [bl_dig2][bl_ch2].type = Pawn;
@@ -37,6 +35,6 @@ int move (char *string, Board *board) {   // Условия измени
 			board -> cell [bl_dig2][bl_ch2].color = Black;
 		}
 	}
-	
+			
 	return 0;
 }
